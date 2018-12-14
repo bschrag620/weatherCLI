b673d8cf2256ec6c73c750bf66a3d4caa91c834c
brad schrag <brad.schrag@gmail.com>
2018-12-13 18:53:14 -0600

    added class methods for adding and removing zips from teh Settings singleton

--- lib/settings.rb
+++ lib/settings.rb
@@ -5,9 +5,7 @@ class Settings
 
     def self.init
         if !File.file?(SETTINGS_PATH)
-            File.open(SETTINGS_PATH, 'w') do |f|
-                f.write(JSON.pretty_generate(self.class_hash))
-            end
+            self.save_zips
         end
         file = File.read(SETTINGS_PATH)
         settings_hash = JSON.parse(file)
@@ -24,6 +22,21 @@ class Settings
 
     def self.reset_zips
         @@zip_code = []
+    end
+
+    def self.save_zips
+        File.open(SETTINGS_PATH, 'w') do |f|
+            f.write(JSON.pretty_generate(@@zip_codes))
+        end
+    end
+
+    def self.add_zip(zip)
+        @@zip_codes << zip
+    end
+
+    def self.remove_zip(zip)
+        @@zip_codes.delete(zip)
+        self.save_zips
     end
 
 end
